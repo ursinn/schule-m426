e8fb6da96dad8fc4d7634ef9769a39349ac71f23
MasterNici <54352264+MasterNIci@users.noreply.github.com>
2019-10-25 15:05:51 +0200

    tabelle

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="de" dir="ltr">
   <head>
@@ -6,6 +8,52 @@
     <title>Passwort Generieren</title>
   </head>
   <body>
+    <?php
+    echo "<td></td>";
+    $a = 1;
+    $b = 2;
+    $c = 3;
+    $d = 4;
+    $e = 5;
+    $f = 6;
+    $g = 7;
+    $h = 8;
+    $k = 9;
+    $l = 10;
 
+    for($x=1; $x<=10; $x++){
+
+      echo "<td>".$x."</td>";
+
+    }
+    for($y=1; $y<=10; $y++){
+      echo "<tr>";
+      echo "<td>".$y."</td>";
+      $a++;
+      echo "<td>".$a."</td>";
+      $b++;
+      echo "<td>".$b."</td>";
+      $c++;
+      echo "<td>".$c."</td>";
+      $d++;
+      echo "<td>".$d."</td>";
+      $e++;
+      echo "<td>".$e."</td>";
+      $f++;
+      echo "<td>".$f."</td>";
+      $g++;
+      echo "<td>".$g."</td>";
+      $h++;
+      echo "<td>".$h."</td>";
+      $k++;
+      echo "<td>".$k."</td>";
+      $l++;
+      echo "<td>".$l."</td>";
+
+      echo "</tr>";
+    }
+
+
+     ?>
   </body>
 </html>
