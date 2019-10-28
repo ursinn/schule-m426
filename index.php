265859946ceb06d438712e2c2ea4d5659331dfe8
MasterNici <54352264+MasterNIci@users.noreply.github.com>
2019-10-28 16:08:18 +0100

    fix data

--- index.php
+++ index.php
@@ -36,35 +36,52 @@
     echo "<td></td>";
 
     $zahlen = 1;
-    $spbu1 = 0;
+    $test = 1;
 
 $abc = 'A';
 
     for($x=1; $x<=22; $x++){
-
+// i löschen
+// l löschen
+// O löschen
+// Q Löschen
+      if($abc=="I" || $abc=="L" || $abc=="O" || $abc=="Q" ){
+        $abc++;
+      }
       echo "<td>".$abc."</td>";
       $abc++;
     }
+    echo "<tr>";
+    echo "<td>".$zahlen."</td>";
+
     for($y=1; $y<=11; $y++){
-      echo "<tr>";
-      echo "<td>".$zahlen."</td>";
-      $zahlen++;
+
 
       if ($result->num_rows > 0) {
           // output data of each row
           while($row = $result->fetch_assoc()) {
-              echo "<td>". $row["ID"]." ". $row["inhalt"]. "</td>";
-          }
+              echo "<td>". $row["inhalt"]. "</td>";
+
+
+              $test++;
+              if($test==23 || $test==45 || $test==67 || $test==89 || $test==111 || $test==133 || $test==155 || $test==177 || $test==199 || $test==221){
+                  $zahlen++;
+                  echo "</tr>";
+                  echo "<td>".$zahlen."</td>";
+              }
+              }
       } else {
           echo "0 results";
+
+
+    }
+
       }
+
       $conn->close();
 
 
 
-      echo "</tr>";
-
-    }
 
 
 
